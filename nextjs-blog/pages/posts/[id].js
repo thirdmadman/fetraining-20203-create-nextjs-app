cd60dc724ac8a8ee0f16507a1f67bbe2fffb71b2
thirdmadman <thirdmadman@gmail.com>
2023-04-26 22:34:32 +0200

    feat: "Polishing the Post Page" step

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,7 +1,10 @@
 import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
+import {getAllPostIds, getPostData} from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({params}) {
   const postData = await getPostData(params.id);
 
   return {
@@ -19,16 +22,21 @@ export async function getStaticPaths() {
   };
 }
 
+
+
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
