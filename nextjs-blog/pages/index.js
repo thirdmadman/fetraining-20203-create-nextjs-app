522c38292121e612f2d5d072057667632926d175
thirdmadman <thirdmadman@gmail.com>
2023-04-26 22:36:02 +0200

    feat: "Polishing the Index Page" step

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-import { getSortedPostsData } from '../lib/posts';
+import {getSortedPostsData} from '../lib/posts';
+
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -13,7 +16,7 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -22,20 +25,19 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Hi there! I’m software engineer / front-end developer / the creator.]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this on <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
